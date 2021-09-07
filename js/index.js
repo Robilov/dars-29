f20690447890da3a3b40ba127af00df7a702625f
Robilov <robilovhumoyun@gmail.com>
2021-09-07 14:37:10 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var intro = alert("Saytimizga xush kelibsiz!");
+var firstName = prompt("Ismingizni kiriting:", "Ismingiz😊");
+var sureName = prompt("Familyangizni kiriting:", "Familyangiz😀");
+var age = prompt("Yoshingizni kiriting:", "Yoshingiz😉");
+var number = prompt("Telefon nomeringiz📱", "+998901234567")
+var feedback = prompt("Nima uchun bizda ishlamoqchisiz🤔");
+var aboutYou = prompt("O'zingiz haqingizda yozing:", "Qisqacha siz haqingizda😎");
+var exitbye = alert("Arizangiz qabul qilindi🙂");
+console.log(` Ismingiz😊: ${firstName}\n Familyangiz😀: ${sureName}\n Yoshingiz😉: ${age}\n Telefon nomer📱: ${number}\n Resume🗃️: ${feedback}\n Siz haqingizda🙂: ${aboutYou}`);
